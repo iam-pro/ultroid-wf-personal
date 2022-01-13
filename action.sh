b8e6f288cc90d886ef009cb52a2983ee062ea47b
Caleb Noble <uniqisuniq@outlook.com>
2022-01-13 11:09:30 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,7 +2,7 @@ git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid && ls
 pip3 --version
-pip3 install -U -r req*
-pip3 install -U -r res*/sta*/opt*
+pip3 install -U -r req* --ignore-installed
+pip3 install -U -r res*/sta*/opt* --ignore-installed
 python3.9 -c "import pyUltroid"
 python3.9 -m py-Ultroid
