db7509236c51ba33b2b78197b468ff086b96cab6
Caleb Noble <uniqisuniq@outlook.com>
2022-03-19 14:05:34 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -3,6 +3,4 @@ cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid && ls
 pip3 install -U -r req* --ignore-installed
 pip3 install -U -r res*/sta*/opt* --ignore-installed
-pip3 install hiredis redis
-cat .env
 python3 -m pyUltroid
